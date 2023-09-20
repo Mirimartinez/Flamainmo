5ce6c30724bc762064eaffabc9c3be0073840301
Miriana Martinez <mirianamartinezok@gmail.com>
2023-09-20 15:48:52 -0300

    remove lamadrid add new properties in index

--- forms/quote.php
+++ forms/quote.php
@@ -1,5 +1,5 @@
 <?php
-  
+
   $receiving_email_address = 'mirianamartinezok@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -14,7 +14,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = 'Request for a quote';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -28,6 +28,7 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['phone'], 'Phone');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
